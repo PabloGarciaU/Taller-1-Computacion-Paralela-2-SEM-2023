f9940d3d3eaa089ad844f06f777487a50f8490b7
Pablo Garcia Urzua <142696188+PabloGarciaU@users.noreply.github.com>
2023-08-24 00:26:12 -0400

    a

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-#include <fstream>
 #include <cstdlib>
 
 using namespace std;
 
+// Variables utilizadas
+int bucle = 0;
+int opcion;
+
+// Funciones utilizadas
+
+void menu(){
+    system("cls");
+    cout<<"---- Menu Taller 1 ----"<<endl;
+    cout<<"1. Generar y resolver un laberinto aleatorio"<<endl;
+    cout<<"2. Salir"<<endl;
+    cin>>opcion;
+    switch(opcion){
+        case 1:
+            system("cls");
+            cout<<"Generar y resolver un laberinto aleatorio"<<endl;
+            break;
+        case 2:
+            system("cls");
+            cout<<"Salir"<<endl;
+            break;
+        default:
+            system("cls");
+            cout<<"Opcion no valida"<<endl;
+            break;
+    }
+}
+
 int main() {
-    return EXIT_SUCCESS;
+   while(bucle == 0){
+       menu();
+   }
 }
