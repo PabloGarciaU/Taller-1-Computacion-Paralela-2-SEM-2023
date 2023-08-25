f0cad29a52bd0c2c468cc228748e52f31fe3df5b
Pablo Garcia Urzua <142696188+PabloGarciaU@users.noreply.github.com>
2023-08-24 23:34:06 -0400

    implementado el algoritmo a utilizar

--- app/main.cpp
+++ app/main.cpp
@@ -16,64 +16,27 @@ int bucle = 0;
 int opcion;
 int filas;
 int columnas;
-int const densidad = 1;
 
-// Funciones utilizadas para crear y resolver el laberinto
+// Funcion para generar y resolver el laberinto, se basa en el algoritmo de busqueda de profundidad
 
-void generaryresolverLaberinto(int filas, int columnas, int densidad){
-    int **matriz;
-	matriz = new int *[filas];
+void solveMaze(int **matriz, int filas, int columnas, int x, int y) {
+    if (x < 0 || y < 0 || x >= columnas || y >= filas)
+        return;
 
-	int FParedes = densidad * 8; 
-	densidad = filas*columnas*densidad / 4;
+    if (matriz[y][x] != 0)
+        return;
 
-	for (int i = 0; i < filas; i++) {
-		matriz[i] = new int[columnas];
-	}
-	//valores
-	for (int i = 0; i < filas; i++) {
-		for (int j = 0; j < columnas; j++) {
-			if (i == 0 || j == 0 || i == filas - 1 || j == columnas - 1) {
-				matriz[i][j] = 1;
-			}
-			else {
-				matriz[i][j] = 0; 
-			}
-		}
-	}
-	//laberinto 
-	srand(time(NULL));
-	for (int i = 0; i < densidad; i++) {
-		int x = rand() % (columnas - 4) + 2; // 2 18 
-		x = (x / 2) * 2; 
-		int y = rand()% (filas - 4) + 2;
-		y = (y / 2) * 2;
-		matriz[y][x] = 1; 
-		for (int j = 0; j < FParedes; j++) {
-			int mx[4] = { x,  x,  x + 2, x - 2 };
-			int my[4] = { y + 2,y - 2, y ,  y };
-			int r = rand() % 4;
-			if (matriz[my[r]][mx[r]] == 0) {
-				matriz[my[r]][mx[r]] = 1; 
-				matriz[my[r] +( y - my[r])/2][mx[r]+(x- mx[r])/2] = 1;
-			}
-				
-		}
-	}
-	//imprimir
-	for (int i = 0; i < filas; i++) {
-		for (int j = 0; j < columnas; j++) {
-			if (matriz[i][j] == 1) {
-				char a = 178; 
-				cout << a << a; 
-			}
-			if(matriz[i][j] == 0) {
-				cout << "  ";
-			}
-		}
-		cout << "\n"; 
-	}
+    matriz[y][x] = 2; // Marcamos como visitado
+
+    solveMaze(matriz, filas, columnas, x + 1, y);
+    solveMaze(matriz, filas, columnas, x - 1, y);
+    solveMaze(matriz, filas, columnas, x, y + 1);
+    solveMaze(matriz, filas, columnas, x, y - 1);
 }
+
+// Funcion para generar y resolver el laberinto
+
+
 
 // Funcion del menu principal
 
@@ -97,7 +60,7 @@ void menu(){
             cin>>filas;
             system("cls");
             cout<<"Generando laberinto..."<<endl;
-			generaryresolverLaberinto(filas, columnas, densidad);
+			//generaryresolverLaberinto(filas, columnas);
             system("pause");
             break;
         case 2:
